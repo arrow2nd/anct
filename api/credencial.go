d3f2b39dcc65b6d69b4cc90bb946701894595df2
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-01-29 18:58:55 +0900

    feat: Revokeする処理を追加

---
+++ api/credencial.go
@@ -0,0 +1,33 @@
+package api
+
+import "net/http"
+
+// Credencial : 認証情報
+type Credencial struct {
+	// AccessToken : トークン
+	AccessToken string `json:"access_token"`
+}
+
+// Revoke : トークンを失効させる
+func (c *Credencial) Revoke() error {
+	req, err := http.NewRequest(http.MethodPost, tokenURL, nil)
+	if err != nil {
+		return err
+	}
+
+	q := req.URL.Query()
+	q.Add("client_id", cliendID)
+	q.Add("client_secret", clientSecret)
+	q.Add("token", c.AccessToken)
+	req.URL.RawQuery = q.Encode()
+
+	req.Header.Add("Authorization", "Bearer "+c.AccessToken)
+
+	client := http.Client{}
+	if _, err := client.Do(req); err != nil {
+		return err
+	}
+
+	c.AccessToken = ""
+	return nil
+}
