9778277f4c048feff8fa9b30a98203f186a7653c
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-01-30 22:09:15 +0900

    fix) errorCodeがPublicになってる #5

--- main.go
+++ main.go
@@ -8,22 +8,26 @@ import (
 	"github.com/arrow2nd/anct/credencial"
 )
 
+type exitCode int
+
 const (
-	ExitCodeOK int = iota
-	ExitCodeErrLoad
-	ExitCodeErrExec
+	exitCodeOK exitCode = iota
+	exitCodeErrLoad
+	exitCodeErrExec
 )
 
 func main() {
 	cred, err := credencial.Load()
 	if err != nil {
-		exitError(err, ExitCodeErrLoad)
+		exitError(err, int(exitCodeErrLoad))
 	}
 
 	c := cmd.New(cred)
 	if err := c.Execute(); err != nil {
-		exitError(err, ExitCodeErrExec)
+		exitError(err, int(exitCodeErrExec))
 	}
+
+	os.Exit(int(exitCodeOK))
 }
 
 func exitError(e error, c int) {
