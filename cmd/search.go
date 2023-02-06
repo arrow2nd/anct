3c986605d3468483a855ee35db53d067b2fc97d3
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-02-06 22:24:27 +0900

    feat(cmd/search): 作品検索を仮実装

--- cmd/search.go
+++ cmd/search.go
@@ -1,11 +1,23 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"context"
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
 
 func (c *Command) newCmdSearch() *cobra.Command {
 	search := &cobra.Command{
 		Use:   "search",
 		Short: "Search for works, characters",
+	}
+
+	works := &cobra.Command{
+		Use:   "works [<query>]",
+		Short: "Search for works",
+		Args:  cobra.ExactArgs(1),
+		RunE:  c.searchWorksRun,
 	}
 
 	characters := &cobra.Command{
@@ -14,16 +26,25 @@ func (c *Command) newCmdSearch() *cobra.Command {
 		Args:  cobra.ExactArgs(1),
 	}
 
-	works := &cobra.Command{
-		Use:   "works [<query>]",
-		Short: "Search for works",
-		Args:  cobra.ExactArgs(1),
-	}
-
 	search.AddCommand(
-		characters,
 		works,
+		characters,
 	)
 
 	return search
+}
+
+func (c *Command) searchWorksRun(cmd *cobra.Command, arg []string) error {
+	ctx := context.Background()
+	list, err := c.api.Client.SearchWorksByKeyword(ctx, arg[0], 5)
+	if err != nil {
+		// TODO: code = 401 のときに You are not logged in. Please run `anct auth login` を返したい
+		return err
+	}
+
+	for _, node := range list.SearchWorks.Nodes {
+		fmt.Printf("Work ID: %d / TITLE: %s / MEDIA: %s / SEASON: %d %s\n", node.AnnictID, node.Title, node.Media.String(), *node.SeasonYear, node.SeasonName.String())
+	}
+
+	return nil
 }
