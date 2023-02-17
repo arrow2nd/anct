f0a9136c517570963c25160facb82a3af4983f36
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-02-17 21:39:00 +0900

    feat(api): 同時実行数を制限

--- api/mutations.go
+++ api/mutations.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/arrow2nd/anct/gen"
 	"golang.org/x/sync/errgroup"
+	"golang.org/x/sync/semaphore"
 )
 
 // UpdateWorkState : 作品の視聴ステータスを更新
@@ -19,21 +20,27 @@ func (a *API) UpdateWorkState(id string, state gen.StatusState) error {
 
 // CreateEpisodeRecords : エピソードの視聴記録を作成
 func (a *API) CreateEpisodeRecords(episodeIDs []string, rating gen.RatingState, comment string) error {
+	const (
+		resourceMax = int64(2)
+		weight      = int64(1)
+	)
+
+	// 同時実行数を制限
+	sem := semaphore.NewWeighted(resourceMax)
 	eg, ctx := errgroup.WithContext(context.Background())
 
 	for _, ID := range episodeIDs {
 		ID := ID
 
+		sem.Acquire(ctx, weight)
+
 		eg.Go(func() error {
-			select {
-			case <-ctx.Done():
-				return nil
-			default:
-				if _, err := a.client.CreateEpisodeRecord(context.Background(), ID, rating, &comment); err != nil {
-					return err
-				}
-				return nil
+			if _, err := a.client.CreateEpisodeRecord(ctx, ID, rating, &comment); err != nil {
+				return err
 			}
+
+			sem.Release(weight)
+			return nil
 		})
 	}
 
