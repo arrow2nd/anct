377f1352088c916b1b8a63a71312c80ae33ccd4c
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-03-07 22:23:21 +0900

    feat(cmdutil): 大文字小文字を区別しないように変更

--- cmdutil/validate.go
+++ cmdutil/validate.go
@@ -3,11 +3,13 @@ package cmdutil
 import (
 	"fmt"
 	"regexp"
+	"strings"
 )
 
 // validateSeasonFormat : シーズン指定の書式が正しいか
 func validateSeasonFormat(s string) error {
 	r := regexp.MustCompile(`^\d{4}-(spring|summer|autumn|winter)$`)
+	s = strings.ToLower(s)
 
 	if !r.MatchString(s) {
 		return fmt.Errorf("incorrect season format (%s)", s)
